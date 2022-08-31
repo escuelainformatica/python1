ef045f020c0611ed261f652d8350b59b0fd7703c
jorgecc <jcastro@eftec.cl>
2022-08-31 09:59:29 -0400

    primera version

---
+++ tabla_productos.py
@@ -0,0 +1,17 @@
+producto={"nombre":"cocacola","precio":200}
+producto2={"nombre":"fanta","precio":400}
+productos=[producto,producto2]
+
+print(productos)
+
+productos = []   # es una lista vacia.
+productos.append(producto)
+productos.append(producto2)
+print(productos)
+
+productos=[
+    {"nombre":"cocacola","precio":200},
+    {"nombre":"fanta","precio":400}
+]
+print(productos)
+
