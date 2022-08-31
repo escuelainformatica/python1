45d4f93f30e55ac141c0242e61ca56dcd3802dd9
jorgecc <jcastro@eftec.cl>
2022-08-31 09:44:30 -0400

    primera version

---
+++ primero.py
@@ -0,0 +1,18 @@
+# primero.py ---> python  --> codigo de maquina (corre la ejecucion)
+
+
+# este es un comentario
+
+print("hola")  # esto es otro comentario
+
+print("este es un texto")  # "esto es un texto"
+print(20)
+print(20.3)
+
+producto = "cocacola"
+print(producto)
+producto = "fanta"
+print(producto)
+
+
+
---
+++ variables.py
@@ -0,0 +1,29 @@
+# ejemplo de variables
+# nombre:
+# producto = ok
+# 40producto = no ok.
+
+productoNombre = "cocacola"
+producto_nombre = "cocacola"
+
+# 40producto="cocacola" no es posible
+
+producto40="cocacola"
+
+producto = "cocacola"  # a la variable producto le asigno el valor "cocacola"
+precio = 2000  # entero
+# las listas son modificables
+paises = ["Chile", "Argentina", "Peru"]  # lista
+precios = [20, 30, 40]
+print(producto)
+print(precio)
+print(paises)
+print(precios)
+paises.append("Bolivia")   # paises es una variable del tipo lista.
+print(paises)
+# las tuplas no pueden modificarse
+paises = ("Chile", "Argentina", "Peru")   # tupla
+print(paises)
+# paises.append("bolivia")
+
+
